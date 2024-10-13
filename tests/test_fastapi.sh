ddfda40d2ab5c0359b719a53b3ceed835d351752
vankhoa21991 <vankhoa21991@gmail.com>
2024-10-13 11:54:04 +0200

    update

--- tests/test_fastapi.sh
+++ tests/test_fastapi.sh
@@ -1,12 +1,12 @@
-# curl -X POST "http://127.0.0.1:8000/chatbot" \
-# -H "Content-Type: application/json" \
-# -d '{
-#     "user_input": "Hello, what is machine learning?",
-#     "model_type": "text",
-#     "selected_model": "Llama-3.2-1B-Instruct-Q4_K_M-GGUF",
-#     "selected_task": "chat",
-#     "image_url": null
-# }'
+curl -X POST "http://127.0.0.1:8000/chatbot" \
+-H "Content-Type: application/json" \
+-d '{
+    "user_input": "Hello, what is machine learning?",
+    "model_type": "text",
+    "selected_model": "Llama-3.2-1B-Instruct-Q4_K_M-GGUF",
+    "selected_task": "chat",
+    "image_url": null
+}'
 
 curl -X POST "http://127.0.0.1:8000/chatbot" \
 -H "Content-Type: application/json" \
@@ -15,10 +15,10 @@ curl -X POST "http://127.0.0.1:8000/chatbot" \
     "model_type": "image",
     "selected_model": "Llava-1.5",
     "selected_task": "chat",
-    "image_url": "data/img.png"
+    "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"
 }'
 
-# # upload image
-# curl -X POST "http://127.0.0.1:8000/upload-image" \
-# -H "Content-Type: multipart/form-data" \
-# -F "file=@data/img.png"
+# upload image
+curl -X POST "http://127.0.0.1:8000/upload-image" \
+-H "Content-Type: multipart/form-data" \
+-F "file=@data/img.png"
